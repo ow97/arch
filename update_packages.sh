3ff46d7cf8b5a509537b05795cb49fd1151dc638
ow97 <ow97@users.noreply.github.com>
2018-09-02 19:32:14 +0100

    Improve package update script

--- update_packages.sh
+++ update_packages.sh
@@ -1,5 +1,17 @@
-echo "Writing native package list..."
+#!/usr/bin/env bash
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${bold}Cleaning out uneeded dependencies...${normal}"
+pacman -Qdtq | sudo pacman -Rns - >/dev/null 2>&1
+
+echo "${bold}Writing native package list...${normal}"
 pacman -Qqettn > ~/git/arch/packages
 
-echo "Writing foreign package list..."
+echo "${bold}Writing foreign package list...${normal}"
 pacman -Qqettm > ~/git/arch/aur_packages
+
+echo "${bold}Committing updated package lists...${normal}"
+git commit -m "Update package lists" packages aur_packages
+
+echo "${bold}Complete! You may want to git push${normal}"
