9686bb62985a6630ddda4937682bd9c502d104dd
ow97 <ow97@users.noreply.github.com>
2019-05-16 20:37:36 +0100

    General improvements to update package lists script

--- update_packages.sh
+++ update_packages.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+cd "${0%/*}"
 if [[ ! -f packages || ! -f aur_packages ]]; then
     echo "One or more of packages and aur_packages doesn't exist."
     echo "Are we in the right directory? Quitting.."
@@ -11,20 +12,29 @@ ORPHANS=$(pacman -Qdtq)
 if [[ -n "${ORPHANS}" ]]; then
     sudo pacman -Rns ${ORPHANS}
 fi
+echo
 
 echo "Checking database..."
-sudo pacman -Dk
+sudo pacman -Dkq
+echo
 
 echo "Checking install consistency..."
 sudo pacman -Qkq
+echo
 
 echo "Writing native package list..."
 pacman -Qqettn > packages
+echo
 
 echo "Writing foreign package list..."
 pacman -Qqettm > aur_packages
-
 echo
+
+if git diff --quiet aur_packages packages; then
+    echo "No changes to commit! Exiting..."
+    exit
+fi
+
 git --no-pager diff -U0 packages aur_packages
 echo
 
